0e6ada7a9a35e4221b83f72daf4b1da2a32a9013
BIVARISH <arsonboy64@gmail.com>
2025-01-23 06:06:35 -0800

    Add files via upload

---
+++ ARRAY.js
@@ -0,0 +1,7 @@
+// Create an array
+let fruits = ["Apple", "Banana", "Cherry"];
+
+// Access elements in the array
+console.log(fruits[0]); // Output: Apple
+console.log(fruits[1]); // Output: Banana
+console.log(fruits[2]); // Output: Cherry
